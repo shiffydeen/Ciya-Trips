bf319edf50b59d8f1474c03adb5dc716f8e58bae
Sheriff Aderombi <ayaderombi@gmail.com>
2025-02-11 11:43:46 +0100

    Ciya Trip

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Home, Layout, TripDetails } from "./utils/Route";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+            path="/details/:id"
+            element={
+              <Layout>
+                <TripDetails />
+              </Layout>
+            }
+          />
+          </Routes>
+        </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
